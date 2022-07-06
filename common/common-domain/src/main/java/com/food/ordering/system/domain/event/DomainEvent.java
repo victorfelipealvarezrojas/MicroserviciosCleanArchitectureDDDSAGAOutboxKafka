e39602e3bf2929763cca40a2874074c06bb8ba56
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2022-07-06 15:05:02 -0400

    nueva version

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/event/DomainEvent.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.domain.event;
+
+// T representa la entidad donse se generara el evento
+// permite desacoplar dominios que se encuentran en distintos contextos
+public interface DomainEvent<T> {
+
+}
